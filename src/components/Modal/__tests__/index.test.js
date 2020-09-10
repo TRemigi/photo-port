9b722b18fd2123367846f60ef1ada570d45ed113
TRemigi <63087524+TRemigi@users.noreply.github.com>
2020-09-10 14:37:48 -0600

    add Modal component and tests

---
+++ src/components/Modal/__tests__/index.test.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Modal from '..';
+
+const mockToggleModal = jest.fn();
+
+const currentPhoto = {
+    name: 'Park bench',
+    category: 'landscape',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+    index: 1
+  };
+
+afterEach(cleanup);
+
+describe('Modal renders', () => {
+    it('renders', () => {
+        render(<Modal 
+        currentPhoto={currentPhoto}
+        onClose={mockToggleModal}
+        />);
+    });
+
+    it('renders', () => {
+        const { asFragment } = render(<Modal 
+            currentPhoto={currentPhoto}
+            onClose={mockToggleModal}
+            />);
+        expect(asFragment()).toMatchSnapshot()
+    });
+});
+
+describe('Click Event', () => {
+    it('calls onClose handler', () => {
+        const { getByText } = render(<Modal 
+            currentPhoto={currentPhoto}
+            onClose={mockToggleModal}
+            />);
+            fireEvent.click(getByText('Close'));
+            expect(mockToggleModal).toHaveBeenCalledTimes(1);
+    });
+})
